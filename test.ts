5f5483a4ea010c84f6d0c1aaad1223226442f241
axetroy <axetroy.dev@gmail.com>
2020-05-06 20:21:58 +0800

    fix ci

--- test.ts
+++ test.ts
@@ -16,8 +16,8 @@ function sleep(ms: number): Promise<void> {
 }
 
 const __filepath = path.normalize(
-  import.meta.url.replace("file://", "").replace(/^\\/, ""),
-);
+  import.meta.url.replace("file://", ""),
+).replace(/^\\/, "");
 
 console.log(`test filename: ${JSON.stringify(__filepath)}`);
 
